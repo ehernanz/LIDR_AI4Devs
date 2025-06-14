ed95f15e43956e46b843d2ade27ea30ef451451f
ehernanz <ehm1984@gmail.com>
2025-06-14 13:05:18 +0200

    Se añade el promp y el código del Bonus 1 y 2

--- reversestring-EHM/prompts.md
+++ reversestring-EHM/prompts.md
@@ -1,8 +1,19 @@
 #ChatGPT 4o
 
+
+#Ejercició inicial
 Necesito que te comportes como un programador experimentado en JavaScript.
 
 Tengo que crear página web en javascript  que contenga un textbox y un botón.
 El comportamiento tiene que sea, que al darle al botón, devuelva el texto que está en el textbox con el orden de los caracteres invertido.
 
 Como salida, sólo necesito dos ficheros: index.html y script.js
+
+
+
+#Bonus1
+Me puedes añadir un juego nuevo de textbox y botón, pero que el boton sólo esté activo cuando haya al menos 3 carácteres en este nuevo textbox?
+
+
+#Bonus2
+Y por último, añádeme otro textbox, está vez sin botón, que a medida se se vaya escribiendo en él, te vaya devolviendo la texto en orden inverso.
--- reversestring-EHM/script.js
+++ reversestring-EHM/script.js
@@ -3,3 +3,21 @@ function reverseText() {
     const reversed = input.split('').reverse().join('');
     document.getElementById('output').textContent = reversed;
 }
+
+function validateInput() {
+    const input = document.getElementById('inputText2').value;
+    const button = document.getElementById('reverseBtn2');
+    button.disabled = input.length < 3;
+}
+
+function reverseText2() {
+    const input = document.getElementById('inputText2').value;
+    const reversed = input.split('').reverse().join('');
+    document.getElementById('output2').textContent = reversed;
+}
+
+function liveReverse() {
+    const input = document.getElementById('inputText3').value;
+    const reversed = input.split('').reverse().join('');
+    document.getElementById('output3').textContent = reversed;
+}
